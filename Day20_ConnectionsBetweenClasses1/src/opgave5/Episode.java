b622692a18883ae8952192067c5ac9dc65cf3b06
Luka2477 <luke.da.duke@hotmail.com>
2021-11-03 11:39:31 +0100

    Opgave 5.2

--- Day20_ConnectionsBetweenClasses1/src/opgave5/Episode.java
+++ Day20_ConnectionsBetweenClasses1/src/opgave5/Episode.java
@@ -3,9 +3,15 @@ package opgave5;
 import java.util.ArrayList;
 
 public class Episode {
-    private int number;
-    private ArrayList<String> guestActors;
-    private int lengthMinutes;
+    private final int number;
+    private final ArrayList<String> guestActors;
+    private final int lengthMinutes;
+
+    public Episode(int number, ArrayList<String> guestActors, int lengthMinutes) {
+        this.number = number;
+        this.guestActors = guestActors;
+        this.lengthMinutes = lengthMinutes;
+    }
 
     public int getNumber () {
         return this.number;
--- Day20_ConnectionsBetweenClasses1/src/opgave5/Series.java
+++ Day20_ConnectionsBetweenClasses1/src/opgave5/Series.java
@@ -6,6 +6,8 @@ public class Series {
     private final String title;
     private final ArrayList<String> cast;
 
+    private final ArrayList<Episode> episodes = new ArrayList<>();
+
     public Series(String title, ArrayList<String> cast) {
         this.title = title;
         this.cast = cast;
@@ -17,5 +19,28 @@ public class Series {
 
     public ArrayList<String> getCast () {
         return new ArrayList<>(this.cast);
+    }
+
+    public void createEpisode (int number, ArrayList<String> guestActors, int lengthMinutes) {
+        Episode episode = new Episode(number, guestActors, lengthMinutes);
+        this.episodes.add(episode);
+    }
+
+    public void removeEpisode (Episode episode) {
+        this.episodes.remove(episode);
+    }
+
+    public Episode getEpisode (int number) {
+        for (Episode episode : this.episodes) {
+            if (episode.getNumber() == number) {
+                return episode;
+            }
+        }
+
+        return null;
+    }
+
+    public ArrayList<Episode> getEpisodes () {
+        return new ArrayList<>(this.episodes);
     }
 }
